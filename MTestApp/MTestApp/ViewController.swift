b8527f8124844989c4b9212ec205fe67b035dcd7
Lazar Andonov <lazar@andonov.biz>
2023-03-02 09:33:16 +0100

    Add xcode project

---
+++ MTestApp/MTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MtestApp
+//
+//  Created by Lazar Andonov on 2.3.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
